f60c9141207bf3e30d5a075fc2938febcc47edd5
mukhashzip666 <mukhammed8021@icloud.com>
2025-11-06 11:55:55 +0500

    Add files via upload

---
+++ kitap.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = 'Informatika';
+  String autor = 'Almaty Kitap';
+  int str = 897;
+  int year = 2019;
+  bool kitaptar = true;
+
+  print('Кітап: $name');
+  print('Автор: $autor');
+  print('Беттер саны: $str');
+  print('Шығарылған жылы: $year');
+  print('Кітап па? $kitaptar');
+}
